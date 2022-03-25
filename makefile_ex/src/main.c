41661927808b56dff022248441fbdf97ba4447e5
st084331 <71183199+st084331@users.noreply.github.com>
2022-03-25 19:59:18 +0300

    Add files via upload

---
+++ makefile_ex/src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "../include/functions.h"
+
+int main(){
+    //error 2
+    print_message();
+    printf("The sum of 5 is %d", sum(5));
+
+    return 0;
+}
---
+++ makefile_ex/src/new_script.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "../include/functions.h"
+
+void print_message() {
+    printf("This is my script!");
+}
